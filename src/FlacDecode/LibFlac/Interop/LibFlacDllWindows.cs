b23b841f667a6daae3d8b09c53bb3462f1e74525
Iain Ballard <iain.ballard@7digital.com>
2013-02-21 16:35:51 +0000

    flac work

--- src/FlacDecode/LibFlac/Interop/LibFlacDllWindows.cs
+++ src/FlacDecode/LibFlac/Interop/LibFlacDllWindows.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using FlacDecode.Flac;
 
 namespace FlacDecode.LibFlac.Interop
 {
@@ -25,10 +26,15 @@ namespace FlacDecode.LibFlac.Interop
 		[DllImport(LibFlac, CallingConvention = CallingConvention.Cdecl)]
 		public static extern void FLAC__stream_decoder_init_stream(
 			IntPtr decoder,
-			Callbacks.Read readCallback,
-			Callbacks.Seek seekCallback,
-			Callbacks.NotImplemented tellCallback, // pass null,
-			Callbacks.Length lengthCallback
+			Callbacks.Read readCallback, // not null
+			Callbacks.Seek seekCallback, // can be null
+			Callbacks.Tell tellCallback, // can be null
+			Callbacks.Length lengthCallback, // can be null
+			Callbacks.Eof eofCallback, // can be null
+			Callbacks.Write writeCallback, // not null
+			Callbacks.Metadata metadataCallback, // can be  null
+			Callbacks.Error errorCallback, //not null
+			IntPtr clientData
 			);
 	}
 
@@ -60,32 +66,78 @@ namespace FlacDecode.LibFlac.Interop
 		/// <param name="clientData">The callee's client data set through FLAC__stream_decoder_init_*().</param>
 		public delegate FlacLengthStatus Length(IntPtr decoder, UInt64* length, IntPtr clientData);
 
+		/// <summary>
+		/// called when the decoder needs to know if the end of the stream has been reached.
+		/// </summary>
+		/// <param name="decoder">The decoder instance calling the callback.</param>
+		/// <param name="clientData">The callee's client data set through FLAC__stream_decoder_init_*()</param>
+		/// <returns>true if the currently at the end of the stream, else false.</returns>
+		[return: MarshalAs(UnmanagedType.Bool)]
+		public delegate bool Eof(IntPtr decoder, IntPtr clientData);
 
-		public delegate void NotImplemented();
+		/// <summary>
+		/// called when the decoder wants to know the current position of the stream. The callback should return the byte offset from the beginning of the stream.
+		/// </summary>
+		/// <param name="decoder">The decoder instance calling the callback</param>
+		/// <param name="absoluteByteOffset">A pointer to storage for the current offset from the beginning of the stream</param>
+		/// <param name="clientData">The callee's client data set through FLAC__stream_decoder_init_*()</param>
+		public delegate FlacTellStatus Tell(IntPtr decoder, UInt64 *absoluteByteOffset, IntPtr clientData);
 
-		// Not yet implemented callback
+		/// <summary>
+		/// Called when the decoder has decoded a single audio frame. The decoder will pass the frame metadata as well as an array of pointers (one for each channel) pointing to the decoded audio.
+		/// </summary>
+		/// <param name="decoder">The decoder instance calling the callback.</param>
+		/// <param name="frame">The description of the decoded frame; DON'T RELY ON THIS DATA.</param>
+		/// <param name="buffer">An array of pointers to decoded channels of data. Each pointer will point to an array of signed samples of length frame->header.blocksize. Channels will be ordered according to the FLAC specification</param>
+		/// <param name="clientData">The callee's client data set through FLAC__stream_decoder_init_*().</param>
+		public delegate FlacWriteStatus Write(IntPtr decoder, FlacFrame frame, Int32[][] buffer, IntPtr clientData);
 
+		/// <summary>
+		/// called when the decoder has decoded a metadata block. In a valid FLAC file there will always be one STREAMINFO block, followed by zero or more other metadata blocks. These will be supplied by the decoder in the same order as they appear in the stream and always before the first audio frame (i.e. write callback). The metadata block that is passed in must not be modified, and it doesn't live beyond the callback, so you should make a copy of it with FLAC__metadata_object_clone() if you will need it elsewhere. Since metadata blocks can potentially be large, by default the decoder only calls the metadata callback for the STREAMINFO block; you can instruct the decoder to pass or filter other blocks with FLAC__stream_decoder_set_metadata_*() calls.
+		/// </summary>
+		/// <param name="decoder">The decoder instance calling the callback. In general, FLAC__StreamDecoder functions which change the state should not be called on the decoder while in the callback.</param>
+		/// <param name="metadata">The decoded metadata block</param>
+		/// <param name="clientData">The callee's client data set through FLAC__stream_decoder_init_*().</param>
+		public delegate void Metadata(IntPtr decoder, IntPtr metadata, IntPtr clientData);
+
+		public delegate void Error(IntPtr decoder, FlacErrorStatus status, IntPtr clientData);
+
+		public enum FlacErrorStatus
+		{
+			FLAC__STREAM_DECODER_ERROR_STATUS_LOST_SYNC,
+			FLAC__STREAM_DECODER_ERROR_STATUS_BAD_HEADER,
+			FLAC__STREAM_DECODER_ERROR_STATUS_FRAME_CRC_MISMATCH,
+			FLAC__STREAM_DECODER_ERROR_STATUS_UNPARSEABLE_STREAM
+		}
+		public enum FlacTellStatus
+		{
+			FLAC__STREAM_DECODER_TELL_STATUS_OK,
+			FLAC__STREAM_DECODER_TELL_STATUS_ERROR,
+			FLAC__STREAM_DECODER_TELL_STATUS_UNSUPPORTED
+		}
 		public enum FlacReadStatus
 		{
 			FLAC__STREAM_DECODER_READ_STATUS_CONTINUE,
 			FLAC__STREAM_DECODER_READ_STATUS_END_OF_STREAM,
 			FLAC__STREAM_DECODER_READ_STATUS_ABORT
 		}
-
 		public enum FlacSeekStatus
 		{
 			FLAC__STREAM_DECODER_SEEK_STATUS_OK,
 			FLAC__STREAM_DECODER_SEEK_STATUS_ERROR,
 			FLAC__STREAM_DECODER_SEEK_STATUS_UNSUPPORTED
 		}
-
 		public enum FlacLengthStatus
 		{
 			FLAC__STREAM_DECODER_LENGTH_STATUS_OK,
 			FLAC__STREAM_DECODER_LENGTH_STATUS_ERROR,
 			FLAC__STREAM_DECODER_LENGTH_STATUS_UNSUPPORTED
 		}
-
+		public enum FlacWriteStatus
+		{
+			FLAC__STREAM_DECODER_WRITE_STATUS_CONTINUE,
+			FLAC__STREAM_DECODER_WRITE_STATUS_ABORT
+		}
 	}
 
 }
